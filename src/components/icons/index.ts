345fe306aa6e74e73a96c85e6b24c7e31c6ede6a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-13 14:41:50 +0100

    chore: add header icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,6 @@
 export { default as Menu } from './menu';
 export { default as Search } from './search';
+export { default as Grid } from './grid';
+export { default as Filter } from './filter';
+export { default as Support } from './support';
+export { default as Setting } from './setting';
---
+++ src/components/icons/plus.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function plus(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 48 48"
+      className="prefix__pTh3n prefix__ydLZ9 prefix__f"
+      {...props}
+    >
+      <path d="M38 26H26v12h-4V26H10v-4h12V10h4v12h12v4z" />
+    </svg>
+  );
+}
+
+export default plus;
